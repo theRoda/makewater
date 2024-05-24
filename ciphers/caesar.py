2570ff5b946a72bd97be07eb20a009ada1ca0f00
theRoda <thisreallylongname@gmail.com>
2024-05-24 03:49:52 -0400

    refactor

--- ciphers/caesar.py
+++ ciphers/caesar.py
@@ -4,7 +4,7 @@ import ciphers.check as check
 def testCaesar(ciphertext):
     for rot in range(1,26):
         alphabet = string.ascii_lowercase
-        shifted_alphabet = alphabet[rot:] + alphabet[:rot]
-        table = str.maketrans(alphabet, shifted_alphabet)
+        shifted = alphabet[rot:] + alphabet[:rot]
+        table = str.maketrans(alphabet, shifted)
         decoded = ciphertext.lower().translate(table)
         check.checkMatch(decoded, rot, 'Caesar')
