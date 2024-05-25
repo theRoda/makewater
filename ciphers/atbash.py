88e29e43886c96b8527fc017e8b1b7408bc90145
theRoda <thisreallylongname@gmail.com>
2024-05-24 20:21:17 -0400

    refactor atbash

--- ciphers/atbash.py
+++ ciphers/atbash.py
@@ -1,13 +1,18 @@
+import string
 import ciphers.check as check
 
 def testAtbash(ciphertext):
-	AtoM = 'ABCDEFGHIJKLM'
-	ZtoN = 'ZYXWVUTSRQPON'
-	decoded = ''
-	for c in ciphertext.upper():
-		if c.isalpha():
-			num = AtoM.find(c) if c in AtoM else ZtoN.find(c)
-			decoded += ZtoN[num] if c in AtoM else AtoM[num]
-		else:
-			decoded += c
+	alphabet = string.ascii_lowercase
+	reversealpha = str.maketrans(alphabet, alphabet[::-1])
+	decoded = ciphertext.lower().translate(reversealpha)
 	check.checkMatch(decoded, None, 'Atbash')
+	# AtoM = 'ABCDEFGHIJKLM'
+	# ZtoN = 'ZYXWVUTSRQPON'
+	# decoded = ''
+	# for c in ciphertext.upper():
+	# 	if c.isalpha():
+	# 		num = AtoM.find(c) if c in AtoM else ZtoN.find(c)
+	# 		decoded += ZtoN[num] if c in AtoM else AtoM[num]
+	# 	else:
+	# 		decoded += c
+	# check.checkMatch(decoded, None, 'Atbash')
