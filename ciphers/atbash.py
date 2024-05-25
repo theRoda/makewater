85360a5bb90689908162711369e2e95d4f5efe80
theRoda <thisreallylongname@gmail.com>
2024-05-24 20:37:28 -0400

    cleanup

--- ciphers/atbash.py
+++ ciphers/atbash.py
@@ -6,13 +6,3 @@ def testAtbash(ciphertext):
 	reversealpha = str.maketrans(alphabet, alphabet[::-1])
 	decoded = ciphertext.lower().translate(reversealpha)
 	check.checkMatch(decoded, None, 'Atbash')
-	# AtoM = 'ABCDEFGHIJKLM'
-	# ZtoN = 'ZYXWVUTSRQPON'
-	# decoded = ''
-	# for c in ciphertext.upper():
-	# 	if c.isalpha():
-	# 		num = AtoM.find(c) if c in AtoM else ZtoN.find(c)
-	# 		decoded += ZtoN[num] if c in AtoM else AtoM[num]
-	# 	else:
-	# 		decoded += c
-	# check.checkMatch(decoded, None, 'Atbash')
